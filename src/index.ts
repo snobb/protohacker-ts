7685b25ad0771e774f175d2e640c4896a6168b81
Alex Kozadaev <akozadaev at yahoo com>
2022-10-12 11:19:27 +0100

    all: change project structure

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,45 @@
-// import { Price } from './price/price';
+// import { RemoteInfo, Socket as UdpSocket, udpServer } from './lib/udp-server';
+import { Chat } from './task03/chat';
 import { FixedChunkStream } from './lib/fixed-chunk-stream';
-import { PriceStream } from './price-stream/price';
-// import { Chat } from './chat/chat';
+import { Price } from './task02a/price';
+import { PriceStream } from './task02b/price';
 import { Socket } from 'node:net';
-// import { prime } from './prime/prime';
-import { tcpServer } from './lib/server';
+import { prime } from './task01/prime';
+import { tcpServer } from './lib/tcp-server';
 
 /* eslint-disable no-console */
 
-const port = 8080;
+const tcpPort = 8080;
 
-function main () {
-    // 03. Budget Chat - https://protohackers.com/problem/3
-    // const chat = new Chat();
+// 00. Smoke Test - https://protohackers.com/problem/0
+export function task00 () {
+    return tcpServer(tcpPort, (conn: Socket) => {
+        conn.pipe(conn);
+    });
+}
 
-    return tcpServer(port, (conn: Socket) => {
-        // 00. Smoke Test - https://protohackers.com/problem/0
-        // conn.pipe(conn);
+// 01. Prime Time - https://protohackers.com/problem/1
+export function task01 () {
+    return tcpServer(tcpPort, (conn: Socket) => {
+        prime(conn);
+    });
+}
 
-        // 01. Prime Time - https://protohackers.com/problem/1
-        // prime(conn);
+// 02. Means to an End - https://protohackers.com/problem/2
+export function task02a () {
+    return tcpServer(tcpPort, async (conn: Socket) => {
+        const price = new Price();
+        await price.handle(conn);
+        conn.destroy();
+    });
+}
 
-        // 02. Means to an End - https://protohackers.com/problem/2
-        // const price = new Price();
-        // await price.handle(conn);
-        // conn.destroy();
-
+// 02. Means to an End - https://protohackers.com/problem/2
+export function task02b () {
+    return tcpServer(tcpPort, (conn: Socket) => {
+        // stream based implementation.
         const price = new PriceStream();
-        const fixedChunkStream = new FixedChunkStream({ size: PriceStream.msgSize, highWaterMark: 9 });
+        const fixedChunkStream = new FixedChunkStream({ size: PriceStream.msgSize });
         conn
             .pipe(fixedChunkStream)
             .pipe(price)
@@ -36,4 +48,12 @@ function main () {
     });
 }
 
-main();
+// 03. Budget Chat - https://protohackers.com/problem/3
+export function task03 () {
+    const chat = new Chat();
+    return tcpServer(tcpPort, (conn: Socket) => {
+        chat.handle(conn);
+    });
+}
+
+task03();
